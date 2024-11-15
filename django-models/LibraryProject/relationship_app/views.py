70fd6e5a715495ec0e080e1b3efa4a87a8c7262b
Eyu-mersha <eyuamersha@gmail.com>
2024-11-16 00:41:10 +0300

    updated

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,6 +8,50 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
 # relationship_app/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponseForbidden
+from .models import Book
+from .forms import BookForm  # Assume you have a form for creating/updating books
+
+# View to add a new book
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')  # Redirect to a book list page (or wherever appropriate)
+    else:
+        form = BookForm()
+
+    return render(request, 'add_book.html', {'form': form})
+
+# View to edit a book
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+
+    return render(request, 'edit_book.html', {'form': form, 'book': book})
+
+# View to delete a book
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')  # Redirect to the book list or another page
+
+    return render(request, 'delete_book.html', {'book': book})
+
+# relationship_app/views.py
 from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponse
 
