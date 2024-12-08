80a0a00efe03b77471bcaaac0a9a56d22786f421
Eyu-mersha <eyuamersha@gmail.com>
2024-12-08 10:53:00 +0300

    updated

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+from .forms import CustomUserCreationForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm(instance=request.user)
+
+    return render(request, 'profile.html', {'form': form})
+
+
+# Use the default LoginView provided by Django
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')  # Redirect to home after successful registration
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
